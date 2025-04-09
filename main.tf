a7c2720f819542b6b97ba3a5b66777764b390e40
cssilva99 <claudia.sofia.msilva@gmail.com>
2025-04-09 12:14:45 +0100

    Added Dynamodb Terraform code

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+locals {
+  enabled = module.this.enabled
+
+  attributes = concat(
+    [
+      {
+        name = var.range_key
+        type = var.range_key_type
+      },
+      {
+        name = var.hash_key
+        type = var.hash_key_type
+      }
+    ],
+    var.dynamodb_attributes
+  )
+
+}
+
+resource "aws_dynamodb_table" "default" {
+  count                       = local.enabled ? 1 : 0
+  name                        = var.table_name
+  billing_mode                = var.billing_mode
+  read_capacity               = var.billing_mode == "PAY_PER_REQUEST" ? null : var.autoscale_min_read_capacity
+  write_capacity              = var.billing_mode == "PAY_PER_REQUEST" ? null : var.autoscale_min_write_capacity
+  hash_key                    = var.hash_key
+  range_key                   = var.range_key
+  stream_enabled              = length(var.replicas) > 0 ? true : var.enable_streams
+  stream_view_type            = length(var.replicas) > 0 || var.enable_streams ? var.stream_view_type : ""
+  table_class                 = var.table_class
+  deletion_protection_enabled = var.deletion_protection_enabled
+}
---
+++ variables.tf
@@ -0,0 +1,91 @@
+variable "region" {
+  type        = string
+  default     = "eu-west-2"
+  description = ""
+}
+
+variable "billing_mode" {
+  type        = string
+  default     = "PROVISIONED"
+  description = "DynamoDB Billing mode. Can be PROVISIONED or PAY_PER_REQUEST"
+}
+
+variable "stream_view_type" {
+  type        = string
+  default     = ""
+  description = "When an item in the table is modified, what information is written to the stream"
+}
+
+variable "replicas" {
+  type        = list(string)
+  default     = []
+  description = "List of regions to create replica"
+}
+
+variable "enable_streams" {
+  type        = bool
+  default     = false
+  description = "Enable DynamoDB streams"
+}
+
+variable "table_name" {
+  type        = string
+  default     = null
+  description = "Table name. If provided, the bucket will be created with this name instead of generating the name from the context"
+}
+
+variable "table_class" {
+  type        = string
+  default     = "STANDARD"
+  description = "DynamoDB storage class of the table. Can be STANDARD or STANDARD_INFREQUENT_ACCESS"
+}
+
+variable "deletion_protection_enabled" {
+  type        = bool
+  default     = false
+  description = "Enable/disable DynamoDB table deletion protection"
+}
+
+variable "autoscale_min_read_capacity" {
+  type        = number
+  default     = 5
+  description = "DynamoDB autoscaling min read capacity"
+}
+
+variable "autoscale_min_write_capacity" {
+  type        = number
+  default     = 5
+  description = "DynamoDB autoscaling min write capacity"
+}
+
+variable "dynamodb_attributes" {
+  type = list(object({
+    name = string
+    type = string
+  }))
+  default     = []
+  description = "Additional DynamoDB attributes in the form of a list of mapped values"
+}
+
+variable "range_key" {
+  type        = string
+  default     = ""
+  description = "DynamoDB table Range Key"
+}
+
+variable "range_key_type" {
+  type        = string
+  default     = "S"
+  description = "Range Key type, which must be a scalar type: `S`, `N`, or `B` for (S)tring, (N)umber or (B)inary data"
+}
+
+variable "hash_key" {
+  type        = string
+  description = "DynamoDB table Hash Key"
+}
+
+variable "hash_key_type" {
+  type        = string
+  default     = "S"
+  description = "Hash Key type, which must be a scalar type: `S`, `N`, or `B` for (S)tring, (N)umber or (B)inary data"
+}
